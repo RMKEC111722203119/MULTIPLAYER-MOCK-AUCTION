f751c85a4bf4795512da0093eeb17fbb4b7e19a8
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2024-12-18 12:45:27 +0530

    sd

--- auc1/app/teams/[slug]/page.tsx
+++ auc1/app/teams/[slug]/page.tsx
@@ -10,36 +10,34 @@ interface PageProps {
   };
 }
 
-const Page = async ({ params }: PageProps) => {
-    const { slug } = params;
-    const fslug = slug.replace(/%20/g, " ");
-    return (
-      <div className="p-8 bg-gray-50 min-h-screen">
-        {/* Title */}
-        <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">
-          {fslug ? `${fslug.toUpperCase()} Franchise Dashboard` : "Franchise Dashboard"}
-        </h1>
-  
-        {/* Grid Layout */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          {/* Purse and Team Info */}
-          <PurseAndTeamInfo
-            team={fslug}  
-          />
-  
-          {/* Bidding Options */}
-          <BidOptions team={fslug}/>
+const Page = ({ params }: PageProps) => {
+  const { slug } = params;
+  const fslug = slug.replace(/%20/g, " ");
+  return (
+    <div className="p-8 bg-gray-50 min-h-screen">
+      {/* Title */}
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">
+        {fslug ? `${fslug.toUpperCase()} Franchise Dashboard` : "Franchise Dashboard"}
+      </h1>
 
-          {/* Retained Players */}
-          <Retention team={fslug} />
-          {/* Purchased Players */}
-          <PurchasedPlayers team={fslug} />
-  
-          {/* Upcoming Players */}
-          <UpcomingPlayers />
-        </div>
+      {/* Grid Layout */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        {/* Purse and Team Info */}
+        <PurseAndTeamInfo team={fslug} />
+
+        {/* Bidding Options */}
+        <BidOptions team={fslug} />
+
+        {/* Retained Players */}
+        <Retention team={fslug} />
+        {/* Purchased Players */}
+        <PurchasedPlayers team={fslug} />
+
+        {/* Upcoming Players */}
+        <UpcomingPlayers />
       </div>
-    );
-  };
-  
-  export default Page;
+    </div>
+  );
+};
+
+export default Page;
