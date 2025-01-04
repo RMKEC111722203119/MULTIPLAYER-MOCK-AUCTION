6f2d3c4c5d9224a1ccad4cd2e4496096f5a83a7d
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2025-01-04 14:24:53 +0530

    Update page.tsx

---
+++ auc/app/comparison/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       <h1> Thanks for visiting the page</h1>
+       <p>This is a testing page go back to the home page </p>
+    </div>
+  )
+}
+
+export default page
